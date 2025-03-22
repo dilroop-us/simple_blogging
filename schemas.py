ab8871d2f4c9c68a599a8edf21aa6d6f910a7122
Dilroop U S <65127204+dilroop-us@users.noreply.github.com>
2025-03-22 12:21:35 +0530

    Error 403 issues

--- schemas.py
+++ schemas.py
@@ -46,10 +46,10 @@ class BlogUpdate(BaseModel):
     imageUrl: Optional[str] = None
     content: Optional[str] = None
 
-    class BlogResponse(Blog):
-        id: str
-        created_at: datetime
-        updated_at: Optional[datetime] = None
+class BlogResponse(Blog):
+    id: str
+    created_at: datetime
+    updated_at: Optional[datetime] = None
 
     model_config = {
         "from_attributes": True
