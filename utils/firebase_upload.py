338fb44bad10fea465470248cd7f76afe9d2bb5d
Dilroop U S <65127204+dilroop-us@users.noreply.github.com>
2025-04-03 12:17:08 +0530

    firebase storage added 2

--- utils/firebase_upload.py
+++ utils/firebase_upload.py
@@ -1,17 +1,26 @@
 from fastapi import UploadFile
 from google.cloud import storage
+from firebase_admin import credentials
 import os
 from dotenv import load_dotenv
+import base64
+import json
 
 load_dotenv()
 
-BUCKET_NAME = os.getenv("FIREBASE_STORAGE_BUCKET")
+# ✅ Load and decode credentials
+firebase_credentials_str = os.getenv("FIREBASE_CREDENTIALS")
+firebase_credentials_json = json.loads(base64.b64decode(firebase_credentials_str).decode())
+bucket_name = os.getenv("FIREBASE_STORAGE_BUCKET")
+
+# ✅ Create credentials object
+firebase_cred = credentials.Certificate(firebase_credentials_json)
 
 def upload_to_firebase(file: UploadFile, folder: str) -> str:
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(BUCKET_NAME)
-    blob = bucket.blob(f"{folder}/{file.filename}")
+    storage_client = storage.Client(credentials=firebase_cred)
+    bucket = storage_client.bucket(bucket_name)
 
+    blob = bucket.blob(f"{folder}/{file.filename}")
     blob.upload_from_file(file.file, content_type=file.content_type)
     blob.make_public()
 
